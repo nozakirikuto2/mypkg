036ee2d2d1b850fb5b5d18b40d025589d1a92ffa
Nozaki Rikuto <s23C1106HE@s.chibakoudai.jp>
2024-12-28 12:38:00 +0900

    Initial commit

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,11 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
+<<<<<<< HEAD
 _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/nozaki/ros2_ws/src/mypkg/install
+=======
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/nozaki/ros2_ws/src/mypkg/mypkg/install
+>>>>>>> Initial commit
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
