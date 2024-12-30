5975f8a310972e1848a16ce626da10c4c3e1e13b
Nozaki Rikuto <s23C1106HE@s.chibakoudai.jp>
2024-12-30 13:43:09 +0900

    try

--- mypkg/launch/install/setup.sh
+++ mypkg/launch/install/setup.sh
@@ -7,11 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-<<<<<<< HEAD
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/nozaki/ros2_ws/src/mypkg/install
-=======
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/nozaki/ros2_ws/src/mypkg/mypkg/install
->>>>>>> Initial commit
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/nozaki/ros2_ws/src/mypkg/launch/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2024 Rikuto Nozaki
+#SPDX-License-Identifier: BSD-3-Clause
+
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -6,7 +9,12 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 181 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+if cat /tmp/mypkg.log | grep -E 'Listen: 360,' /tmp/mypkg.log; then
+	echo "Error: 360以上はないです"
+	exit 1
+else
+	echo 'OK'
+	exit 0
+fi
